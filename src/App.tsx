1c968608bb4f304e4a24402144325cdc64e4e252
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-24 17:02:55 +0100

    Move landing page to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import LandingPage from "./components/LandingPage";
+import { useState } from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 import "./styles/App.css";
 
 function App() {
+  // Todo: redirect to home page if still logged in by checking if a token exists in local storage
+  const [showLogin, setShowLogin] = useState(true);
   return (
     <>
-      <LandingPage />
+      <section className="hero is-link is-fullheight">
+        <div className="hero-body">
+          <div className="columns is-vcentered ps-landing-page">
+            <div className="column is-two-fifths has-text-centered">
+              <span className="icon is-large">
+                <i className="fas fa-bolt"></i>
+              </span>
+              <p className="title">Party Stack</p>
+              <p className="subtitle">
+                An online repository of indoor party games
+              </p>
+            </div>
+            <div className="column">
+              {showLogin ? (
+                <Login setShowLogin={setShowLogin} />
+              ) : (
+                <Register setShowLogin={setShowLogin} />
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
