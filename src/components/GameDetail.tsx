062c97602d479a37ba8518889b90e3529b096839
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-25 16:00:08 +0100

    Enable functionality to add to or remove from favourites

--- src/components/GameDetail.tsx
+++ src/components/GameDetail.tsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { IGame } from "../interfaces/game";
+import { IUser } from "../interfaces/user";
 
 function GameDetail() {
   const [game, setGame] = useState<IGame | null>(null);
+  const [isUserFavourite, setIsUserFavourite] = useState<boolean>(false);
+
   const { gameId } = useParams();
 
   useEffect(() => {
@@ -26,6 +29,64 @@ function GameDetail() {
       void fetchGame();
     }
   }, [gameId]);
+
+  useEffect(() => {
+    // check and set if game is part of the current User's favourites
+    const token = localStorage.getItem("token");
+
+    function isCurrentGameAFavourite(favourites: IGame[]) {
+      return favourites.some((game) => game._id === gameId);
+    }
+
+    async function fetchCurrentUser() {
+      try {
+        const URL = "/api/user";
+        const response = await axios.get<IUser>(URL, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        if (response.data.favourites) {
+          if (isCurrentGameAFavourite(response.data.favourites)) {
+            setIsUserFavourite(true);
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    if (token) {
+      void fetchCurrentUser();
+    }
+  }, [gameId]);
+
+  async function toggleFavourite() {
+    // Add or remove from Favourites
+    const token = localStorage.getItem("token");
+
+    if (isUserFavourite) {
+      try {
+        const URL = `/api/favourites/${gameId}`;
+        await axios.delete(URL, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setIsUserFavourite(false);
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      try {
+        const URL = "/api/favourites";
+        const data = { favourites: gameId };
+        await axios.post(URL, data, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setIsUserFavourite(true);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
 
   return (
     <>
@@ -67,12 +128,20 @@ function GameDetail() {
           <p className="is-size-7 ps-inline margin-right-1x">
             Created by: {game?.creator.username}
           </p>
-          <span className="icon">
-            <i className="fas fa-heart"></i>
-          </span>
-          <span className="icon">
-            <i className="far fa-heart"></i>
-          </span>
+          <div
+            className="ps-inline hover-pointer"
+            onClick={() => void toggleFavourite()}
+          >
+            {isUserFavourite ? (
+              <span className="icon">
+                <i className="fas fa-heart"></i>
+              </span>
+            ) : (
+              <span className="icon">
+                <i className="far fa-heart"></i>
+              </span>
+            )}
+          </div>
         </div>
       </section>
       <hr />
